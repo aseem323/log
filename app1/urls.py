9f91d8b2789794a14a850426044a897180b82076
aseemedakkad <aseemedakkad323@gmail.com>
2021-10-01 13:51:12 +0530

    seven

--- app1/urls.py
+++ app1/urls.py
@@ -3,5 +3,6 @@ from .import views
 
 urlpatterns = [
     path('test',views.testfn,name='test'),
+    path('test2',views.test2fn,name='test2'),
 
 ]
--- app1/views.py
+++ app1/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def testfn(request):
     return render(request,'login.html')
+
+def test2fn(request):
+    return render(request,'log2.html')
